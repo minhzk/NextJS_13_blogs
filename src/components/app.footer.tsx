d19807fc05e57116ccd323443d87a40514e7b8a1
Nguyễn Bình Minh <nbinhminh158@gmail.com>
2024-10-02 21:55:34 +0700

    feat: finish project nextjs 13

---
+++ src/components/app.footer.tsx
@@ -0,0 +1,11 @@
+const AppFooter = () => {
+    return (
+        <>
+            <div className="px-4 flex justify-center items-center bg-gray-500 text-white fixed bottom-0 w-full h-10">
+                <span>AppFooter by Minh Nguyen SEN in 3 years later</span>
+            </div>
+        </>
+    )
+}
+
+export default AppFooter;
